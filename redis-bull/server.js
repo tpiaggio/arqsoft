9be0cdc4287ef533b51ba0344c211db369934439
Tomas Piaggio <tomas@createthrive.com>
2023-04-12 10:52:00 -0300

    added queue processor to same file

--- redis-bull/server.js
+++ redis-bull/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const redis = require('redis');
 const Queue = require('bull');
 
 // Create a new Bull queue
-const myQueue = new Queue('myQueue');
+const redisClient = redis.createClient();
+const myQueue = new Queue('myQueue', { redis: redisClient });
+
+//Bull job processor
+myQueue.process(async (job) => {
+  console.log(`Processing job ${job.id}`);
+  // Do some async work here
+  // ...
+  console.log(`Job ${job.id} completed`);
+});
 
 // Create an Express app
 const app = express();
